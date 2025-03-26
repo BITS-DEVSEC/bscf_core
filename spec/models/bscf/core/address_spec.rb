bbf79ae0f759469c5a0b1a1d6831d1d10691c786
Asrat <asratextras77@gmail.com>
2025-03-26 11:30:51 +0300

    Add model Address - see sup-184-create-address-model

---
+++ spec/models/bscf/core/address_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+module Bscf::Core
+  RSpec.describe Address, type: :model do
+    attributes = []
+    include_examples("model_shared_spec", :address, attributes)
+
+  end
+end
