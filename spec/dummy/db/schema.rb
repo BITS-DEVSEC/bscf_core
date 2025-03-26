55469dffaccca558ae149c17139e1afee856a5b5
Asrat <asratextras77@gmail.com>
2025-03-26 11:32:32 +0300

    Add model UserProfile see - sup-181-create-user-model

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,14 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_26_075111) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_26_081052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "bscf_core_addresses", force: :cascade do |t|
+    t.string "city"
+    t.string "sub_city"
+    t.string "woreda"
+    t.string "latitude"
+    t.string "longitude"
+    t.string "house_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "bscf_core_roles", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bscf_core_user_profiles", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.date "date_of_birth", null: false
+    t.string "nationality", null: false
+    t.string "occupation", null: false
+    t.string "source_of_funds", null: false
+    t.integer "kyc_status", default: 0
+    t.integer "gender", null: false
+    t.datetime "verified_at"
+    t.bigint "verified_by_id"
+    t.bigint "address_id", null: false
+    t.string "fayda_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index [ "address_id" ], name: "index_bscf_core_user_profiles_on_address_id"
+    t.index [ "user_id" ], name: "index_bscf_core_user_profiles_on_user_id"
+    t.index [ "verified_by_id" ], name: "index_bscf_core_user_profiles_on_verified_by_id"
   end
 
   create_table "bscf_core_users", force: :cascade do |t|
@@ -32,4 +62,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_03_26_075111) do
     t.index [ "email" ], name: "index_bscf_core_users_on_email", unique: true
     t.index [ "phone_number" ], name: "index_bscf_core_users_on_phone_number", unique: true
   end
+
+  add_foreign_key "bscf_core_user_profiles", "bscf_core_addresses", column: "address_id"
+  add_foreign_key "bscf_core_user_profiles", "bscf_core_users", column: "user_id"
+  add_foreign_key "bscf_core_user_profiles", "bscf_core_users", column: "verified_by_id"
 end
---
+++ spec/models/bscf/core/user_profile_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+module Bscf
+  module Core
+    RSpec.describe UserProfile, type: :model do
+      attributes = [
+        { date_of_birth: :presence },
+        { nationality: :presence },
+        { occupation: :presence },
+        { source_of_funds: :presence },
+        { gender: :presence },
+        { user: :belong_to },
+        { address: :belong_to }
+      ]
+      include_examples("model_shared_spec", :user_profile, attributes)
+
+      it { is_expected.to belong_to(:verified_by).class_name('User').optional }
+    end
+  end
+end
