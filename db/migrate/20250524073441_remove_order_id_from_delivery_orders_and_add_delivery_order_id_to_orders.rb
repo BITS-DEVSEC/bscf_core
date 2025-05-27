7a224681c0ee42582eb89fcd1770dcbdb5b851bd
Asrat <asratextras77@gmail.com>
2025-05-27 12:57:23 +0300

    Rename invalid migration file

--- db/migrate/20250524073441_remove_order_id_from_delivery_orders_and_add_delivery_order_id_to_orders.rb
+++ db/migrate/20250524073441_remove_order_id_from_delivery_orders_and_add_delivery_order_id_to_orders.rb
@@ -1,4 +1,4 @@
-class RemoveOrderIdFromBscfCoreDeliveryOrdersAndAddDeliveryOrderIdToBscfCoreOrders < ActiveRecord::Migration[8.0]
+class RemoveOrderIdFromDeliveryOrdersAndAddDeliveryOrderIdToOrders < ActiveRecord::Migration[8.0]
   def change
     remove_reference :bscf_core_delivery_orders, :order, foreign_key: { to_table: :bscf_core_orders }, index: true
     add_reference :bscf_core_orders, :delivery_order, foreign_key: { to_table: :bscf_core_delivery_orders }, index: true, null: true
