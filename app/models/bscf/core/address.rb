9fdf5fb3e280b50e5bab6031223452df363058b1
Asrat <asratextras77@gmail.com>
2025-06-24 09:37:36 +0300

    Fix lint errors

--- app/models/bscf/core/address.rb
+++ app/models/bscf/core/address.rb
@@ -11,7 +11,7 @@ module Bscf
       validates :latitude, :longitude, presence: true, if: :requires_coordinates?
 
       def coordinates
-        [latitude.to_f, longitude.to_f] if latitude.present? && longitude.present?
+        [ latitude.to_f, longitude.to_f ] if latitude.present? && longitude.present?
       end
 
       def full_address
